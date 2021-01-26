f4299802262f14357d9e39fb492ae9044f69ab4d
e2f5db0 <e2f5db0@protonmail.com>
2021-01-26 15:40:19 +0200

    Add an arrow to list items & hover effect

--- src/components/CarparkList.js
+++ src/components/CarparkList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import green from '../green.png'
 import yellow from '../yellow.png'
 import red from '../red.png'
+import next from '../next.png'
 
 const CarparkList = ({ carpark, render_carpark }) => {
 
@@ -10,12 +11,20 @@ const CarparkList = ({ carpark, render_carpark }) => {
             <div className='App-list-item' onClick={() => {
                 render_carpark(carpark)
             }}>
-                <div className='Carpark-list-item-upper-row'>
-                    <p>{carpark.name}: </p>
-                    <img className='App-availability-icon' src={green} alt='availability: green'></img>
+                <div className='App-list-item-left-section'>
+                    <div className='Carpark-list-item-upper-row'>
+                        <p>{carpark.name}: </p>
+                    </div>
+                    <div className='Carpark-list-item-middle-row'>
+                        <p className='Availability'>Free</p>
+                        <img className='App-availability-icon' src={green} alt='availability: green'></img>
+                    </div>
+                    <div className='Carpark-list-item-upper-row'>
+                        <p className='Subnotation'>Write the address into your own navigator</p>
+                    </div>
                 </div>
-                <div className='Carpark-list-item-upper-row'>
-                    <p className='Subnotation'>Write the address into your own navigator</p>
+                <div className='App-list-item-right-section'>
+                    <img className='App-next-icon' src={next} alt='arrow pointing right'></img>
                 </div>
             </div>
         )
@@ -26,12 +35,20 @@ const CarparkList = ({ carpark, render_carpark }) => {
             <div className='App-list-item' onClick={() => {
                 render_carpark(carpark)
             }}>
-                <div className='Carpark-list-item-upper-row'>
-                    <p>{carpark.name}: </p>
-                    <img className='App-availability-icon' src={yellow} alt='availability: yellow'></img>
+                <div className='App-list-item-left-section'>
+                    <div className='Carpark-list-item-upper-row'>
+                        <p>{carpark.name}: </p>
+                    </div>
+                    <div className='Carpark-list-item-middle-row'>
+                        <p className='Availability'>Half-full</p>
+                        <img className='App-availability-icon' src={yellow} alt='availability: yellow'></img>
+                    </div>
+                    <div className='Carpark-list-item-upper-row'>
+                        <p className='Subnotation'>Write the address into your own navigator</p>
+                    </div>
                 </div>
-                <div className='Carpark-list-item-upper-row'>
-                    <p className='Subnotation'>Write the address into your own navigator</p>
+                <div className='App-list-item-right-section'>
+                    <img className='App-next-icon' src={next} alt='arrow pointing right'></img>
                 </div>
             </div>
         )
@@ -42,12 +59,20 @@ const CarparkList = ({ carpark, render_carpark }) => {
             <div className='App-list-item' onClick={() => {
                 render_carpark(carpark)
             }}>
-                <div className='Carpark-list-item-upper-row'>
-                    <p>{carpark.name}: </p>
-                    <img className='App-availability-icon' src={red} alt='availability: red'></img>
+                <div className='App-list-item-left-section'>
+                    <div className='Carpark-list-item-upper-row'>
+                        <p>{carpark.name}: </p>
+                    </div>
+                    <div className='Carpark-list-item-middle-row'>
+                        <p className='Availability'>Full</p>
+                        <img className='App-availability-icon' src={red} alt='availability: red'></img>
+                    </div>
+                    <div className='Carpark-list-item-upper-row'>
+                        <p className='Subnotation'>Write the address into your own navigator</p>
+                    </div>
                 </div>
-                <div className='Carpark-list-item-upper-row'>
-                    <p className='Subnotation'>Write the address into your own navigator</p>
+                <div className='App-list-item-right-section'>
+                    <img className='App-next-icon' src={next} alt='arrow pointing right'></img>
                 </div>
             </div>
         )
